b6908d103858bd704dbc090b4a851bc0ba424f4a
BenCrawford1 <bencrawford345@gmail.com>
2020-07-20 19:37:37 +1000

    Adjusted Board.py

--- Board.py
+++ Board.py
@@ -12,10 +12,40 @@ class Board():
         
     def Click(self, squareX, squareY):
         # Change all squares in same column and row as squareX and Y
-        self.board[1][2].Flip()
+       
+         if (mouseButton == LEFT):
+            
+            self.board[x][1].Flip()
+            
+            self.board[x][2].Flip()
+            
+            self.board[x][3].Flip()
+            
+            self.board[x][4].Flip()
+            
+            self.board[1][y].Flip()
+            
+            self.board[2][y].Flip()
+            
+            self.board[3][y].Flip()
+            
+            self.board[4][y].Flip()
+            
+            
         
     def Display(self):
-        self.board[i][j].Display()
+
+    for i in range(5)  #1=<i=<4
+    
+        self.board[i][1].Display()
+        self.board[i][2].Display()
+        self.board[i][3].Display()
+        self.board[i][4].Display()
         
     def Reset(self):
-        self.board[i][j].colour = "White"
+        
+        for i in range(5)  #1=<i=<4
+        self.board[i][1].colour = "White"
+        self.board[i][2].colour = "White"
+        self.board[i][3].colour = "White"
+        self.board[i][4].colour = "White"
