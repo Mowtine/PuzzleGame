cd1d4e49f98ac27ca283219d562445e1b482eb3e
Charlie Torrible <charlietorrible@MacBook-Air.localdomain>
2020-08-02 10:44:50 +1000

    button class

--- Button.py
+++ Button.py
@@ -1,1 +1,29 @@
 # Button class! It needs Display, Click, IsIn functions, and is exactly like a square class otherwise
+class Button():
+    
+    def __init__(self, x, y, h, w):
+        self.colour = "Yellow"
+        self.x = x
+        self.y = y
+        self.h = h
+        self.w = w
+        self.Text = "Button"
+        
+    def Text(self):
+        textAlign(CENTER)
+        text(self.Text,self.x - self.w/2,self.y - self.h/2)
+        
+    def Display(self):
+        if self.colour == "Yellow":
+            fill(255, 204, 0)
+        else:
+            fill(0)
+        rect(self.x, self.y, self.h, self.w,15)
+        
+        
+    def IsIn(self, x, y):
+        if x > self.x - self.w/2 and x < self.x + self.w/2 and y > self.y - self.h/2 and y < self.y + self.h/2:
+            return True
+        else:
+            return False
+        
--- MainMenu.py
+++ MainMenu.py
@@ -1,1 +1,4 @@
 # Main menu is basically like a Board class - copy Board.py It needs a variable which is a list of buttons. The buttons will be added manually. It will need a GetButton, Click and Display
+from Button import Button
+
+ 
