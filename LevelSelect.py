3255668fc36027a4675ad5927394ca1c5635e8c5
BenCrawford1 <bencrawford345@gmail.com>
2020-12-22 20:41:10 +1100

    for loop for button levels

--- LevelSelect.py
+++ LevelSelect.py
@@ -10,33 +10,13 @@ class LevelSelect():
         self.ButtonHeight = 120
         
         # TASK 2: Change and add buttons for each level: Level Valunes: "1.1 level"
-        self.Button1 = Button(x, 180, self.ButtonHeight, self.ButtonWidth, "1.1", "1.1 level", 26)
-        self.Button2 = Button(x + (x/2+20), 180, self.ButtonHeight, self.ButtonWidth, "1.2", "1.2", 26)
-        self.Button3 = Button(x - (x/2+20), 180, self.ButtonHeight, self.ButtonWidth, "1.0", "1", 26)
-        self.Button4 = Button(x, 340, self.ButtonHeight, self.ButtonWidth, "2.1", "2.1", 26)
-        self.Button5 = Button(x + (x/2+20), 340, self.ButtonHeight, self.ButtonWidth, "2.2", "2.2", 26)
-        self.Button6 = Button(x - (x/2+20), 340, self.ButtonHeight, self.ButtonWidth, "2.0", "2", 26)
-        self.Button7 = Button(x, 500, self.ButtonHeight, self.ButtonWidth, "3.1", "3.1", 26)
-        self.Button8 = Button(x + (x/2+20), 500, self.ButtonHeight, self.ButtonWidth, "3.2", "3.2", 26)
-        self.Button9 = Button(x - (x/2+20), 500, self.ButtonHeight, self.ButtonWidth, "3.0", "3", 26)
-        self.Button10 = Button(x, 660, self.ButtonHeight, self.ButtonWidth, "4.1", "4.1", 26)
-        self.Button11 = Button(x + (x/2+20), 660, self.ButtonHeight, self.ButtonWidth, "4.2", "4.2", 26)
-        self.Button12 = Button(x - (x/2+20), 660, self.ButtonHeight, self.ButtonWidth, "4.0", "4", 26)
-        # Add buttons
         
-        self.ButtonsList.append(self.Button1)
-        self.ButtonsList.append(self.Button2)
-        self.ButtonsList.append(self.Button3)
-        self.ButtonsList.append(self.Button4)
-        self.ButtonsList.append(self.Button5)
-        self.ButtonsList.append(self.Button6)
-        self.ButtonsList.append(self.Button7)
-        self.ButtonsList.append(self.Button8)
-        self.ButtonsList.append(self.Button9)
-        self.ButtonsList.append(self.Button10)
-        self.ButtonsList.append(self.Button11)
-        self.ButtonsList.append(self.Button12)
-        # Add them to the list
+        for i in range(1,5):
+            y= 20 + i*160
+
+            for j in range(0,3):    
+                self.ButtonsList.append(Button(x - (x/2+20) + (x/2+20)*j, y, self.ButtonHeight, self.ButtonWidth, str(i)+"."+str(j), str(i)+"." + str(j) + " level", 26))
+
         
     def Display(self):
         for Button in self.ButtonsList:
